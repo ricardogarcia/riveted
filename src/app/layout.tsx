3a2b00c9b87e841c2e017941ad26bcf55858d51f
ricardogarcia <ricardo.garcia@alum.mit.edu>
2026-03-21 13:58:59 -0700

    Add Intercom integration and update .env.example

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { DM_Sans, Bebas_Neue, Instrument_Serif } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -57,6 +58,38 @@ export default function RootLayout({
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
+
+        <Script
+          id="intercom"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.intercomSettings = {
+                app_id: "${process.env.NEXT_PUBLIC_INTERCOM_APP_ID || ""}"
+              };
+              (function(){
+                var w=window;var ic=w.Intercom;
+                if(typeof ic==="function"){
+                  ic('reattach_activator');
+                  ic('update',w.intercomSettings);
+                } else {
+                  var d=document;var i=function(){i.c(arguments);};
+                  i.q=[];i.c=function(args){i.q.push(args);};
+                  w.Intercom=i;
+                  var l=function(){
+                    var s=d.createElement('script');
+                    s.type='text/javascript';s.async=true;
+                    s.src='https://widget.intercom.io/widget/${process.env.NEXT_PUBLIC_INTERCOM_APP_ID || ""}';
+                    var x=d.getElementsByTagName('script')[0];
+                    x.parentNode.insertBefore(s,x);
+                  };
+                  if(document.readyState==='complete'){l();}
+                  else{w.addEventListener('DOMContentLoaded',l,false);}
+                }
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
